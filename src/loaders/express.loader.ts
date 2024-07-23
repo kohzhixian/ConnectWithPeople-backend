1f72883505f01baf17ff0616e0fec5d0083c4fb2
Dexter Chan <dexter.chan@fdmgroup.com>
2024-07-24 01:14:07 +0800

    added another ip address for cors

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -11,7 +11,7 @@ export default function expressLoader() {
   // middleware that will parse the cookie header on the request
   app.use(cookieParser());
   // enables Cross-Origin Resource sharing
-  const allowedOrigins = ["http://localhost:5173"];
+  const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
   app.use(
     cors({
       origin: allowedOrigins,
