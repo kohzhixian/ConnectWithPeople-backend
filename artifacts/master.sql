ba061ea0596fcce60f6bbd84bc1ec84ac2882472
reactPrac <zhixianwork@gmail.com>
2024-09-07 21:06:21 +0800

    added sql for inserting data into chatroom and user_chatroom table

--- artifacts/master.sql
+++ artifacts/master.sql
@@ -20,3 +20,29 @@ INSERT INTO contacts_users (contacts_id, user_id) VALUES ('b395c56c-73e6-4c47-bf
 INSERT INTO contacts_users (contacts_id, user_id) VALUES ('068c8d4e-1923-4079-a38e-6742f4d3fb79', 'cb7d9650-45ab-11ef-af05-401c8383fa1c');
 INSERT INTO contacts_users (contacts_id, user_id) VALUES ('a07798b4-9c99-4dc1-ae01-726cccf57cdd', 'cb7d9650-45ab-11ef-af05-401c8383fa1c');
 INSERT INTO contacts_users (contacts_id, user_id) VALUES ('3f6217d3-62d7-46d3-9a01-146d80d94423', 'cb7d9650-45ab-11ef-af05-401c8383fa1c');
+
+
+-- Insert data into chatroom table
+INSERT INTO chatroom (id, chatroom_name, chatroom_icon, created_at, updated_at) VALUES ('5f6ca4ff-97b0-4466-9d15-0d31bbf8cd05', 'chatroom with alfred', 'chatroom icon', NOW(), NOW());
+INSERT INTO chatroom (id, chatroom_name, chatroom_icon, created_at, updated_at) VALUES ('72673bc8-6292-436c-99d8-0fe6a6b71aaa', 'chatroom with dex', 'chatroom icon', NOW(), NOW());
+INSERT INTO chatroom (id, chatroom_name, chatroom_icon, created_at, updated_at) VALUES ('ba82627e-4989-4009-97cc-a906313cdd4e', 'chatroom with ben', 'chatroom icon', NOW(), NOW());
+INSERT INTO chatroom (id, chatroom_name, chatroom_icon, created_at, updated_at) VALUES ('893563f6-e4f1-4663-87c3-e733b73e1cc1', 'chatroom with dano', 'chatroom icon', NOW(), NOW());
+INSERT INTO chatroom (id, chatroom_name, chatroom_icon, created_at, updated_at) VALUES ('6d6a7237-58a9-4540-b439-49173d5e3225', 'chatroom with huge', 'chatroom icon', NOW(), NOW());
+
+
+-- Insert data into user_chatroom table
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d9650-45ab-11ef-af05-401c8383fa1c', '5f6ca4ff-97b0-4466-9d15-0d31bbf8cd05');
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7ab05a-45ab-11ef-af05-401c8383fa1c', '5f6ca4ff-97b0-4466-9d15-0d31bbf8cd05');
+
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d9650-45ab-11ef-af05-401c8383fa1c', '72673bc8-6292-436c-99d8-0fe6a6b71aaa');
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7b5ab7-45ab-11ef-af05-401c8383fa1c', '72673bc8-6292-436c-99d8-0fe6a6b71aaa');
+
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d9650-45ab-11ef-af05-401c8383fa1c', 'ba82627e-4989-4009-97cc-a906313cdd4e');
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7c0d39-45ab-11ef-af05-401c8383fa1c', 'ba82627e-4989-4009-97cc-a906313cdd4e');
+
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d9650-45ab-11ef-af05-401c8383fa1c', '893563f6-e4f1-4663-87c3-e733b73e1cc1');
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7ca708-45ab-11ef-af05-401c8383fa1c', '893563f6-e4f1-4663-87c3-e733b73e1cc1');
+
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d9650-45ab-11ef-af05-401c8383fa1c', '6d6a7237-58a9-4540-b439-49173d5e3225');
+INSERT INTO user_chatrooms (user_id, chatroom_id) VALUES ('cb7d1b7b-45ab-11ef-af05-401c8383fa1c', '6d6a7237-58a9-4540-b439-49173d5e3225');
+
